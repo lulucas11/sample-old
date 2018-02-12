4efea583b4187184e522e71b8093e2da6f7b3dd9
lucas <lulucas11@foxmail.com>
2018-02-12 07:39:18 +0000

    Finish static pages

--- app/Http/Controllers/StaticPagesController.php
+++ app/Http/Controllers/StaticPagesController.php
@@ -8,30 +8,16 @@ class StaticPagesController extends Controller
 {
     public function home()
     {
-<<<<<<< HEAD
         return view('static_pages/home');
-=======
-        return '主页';
->>>>>>> static-pages
     }
 
     public function help()
     {
-<<<<<<< HEAD
         return view('static_pages/help');
-=======
-        return '帮助页';
->>>>>>> static-pages
     }
 
     public function about()
     {
-<<<<<<< HEAD
         return view('static_pages/about');
     }
 }
-=======
-        return '关于页';
-    }
-}
->>>>>>> static-pages
--- routes/web.php
+++ routes/web.php
@@ -3,24 +3,3 @@
 Route::get('/', 'StaticPagesController@home');
 Route::get('/help', 'StaticPagesController@help');
 Route::get('/about', 'StaticPagesController@about');
-<<<<<<< HEAD
-
-=======
->>>>>>> static-pages
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-<<<<<<< HEAD
-=======
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
->>>>>>> static-pages
