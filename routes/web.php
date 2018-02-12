423a0dee8ec8c5ebf4ad5a6b44aed978b402f6dc
lucas <lulucas11@foxmail.com>
2018-02-12 05:24:32 +0000

    Initial commit

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+Route::get('/', 'StaticPagesController@home');
+Route::get('/help', 'StaticPagesController@help');
+Route::get('/about', 'StaticPagesController@about');
 /*
 |--------------------------------------------------------------------------
 | Web Routes
